d61293365db60595b3f479042896f916b8798670
ayonbit <ayonbit@gmail.com>
2026-05-23 19:47:04 +0600

    Production Build Final

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 
-// import { PrismaClient } from "../../generated/prisma/client";
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as typeof globalThis & {
